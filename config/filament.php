3b0be9c0c8dbdaea07f809d25804ad6612fc3c56
Brem0317 <patrickbarcelo17@gmail.com>
2023-02-10 08:15:34 +0800

    filament components

--- config/filament.php
+++ config/filament.php
@@ -241,15 +241,15 @@ return [
             'have_inline_labels' => false,
         ],
         'footer' => [
-            'should_show_logo' => true,
+            'should_show_logo' => false,
         ],
-        'max_content_width' => null,
+        'max_content_width' => 'full',
         'notifications' => [
             'vertical_alignment' => 'top',
             'alignment' => 'right',
         ],
         'sidebar' => [
-            'is_collapsible_on_desktop' => false,
+            'is_collapsible_on_desktop' => true,
             'groups' => [
                 'are_collapsible' => true,
             ],
